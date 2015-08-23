991a9aeafd6da7b1efc139c6733a4c9d0bb133dc
Allen Kim <allenhwkim@gmail.com>
2015-08-23 01:25:10 -0400

    test updated

--- spec.js
+++ spec.js
@@ -12,33 +12,42 @@ assert.equal(ht("{{x.foo}} {{ x.bar }}", {x: {foo:1, bar: 2}}), "1 2");
 /*******************************************************
  * `ht-if` expression test
  *******************************************************/
+console.log(1);
 assert.equal("<div>YES</div>",
   ht("<div ht-if='x.foo'>YES</div>", {x:{foo:true}}));
+console.log(2);
 assert.equal("",
   ht("<div ht-if='x.bar'>YES</div>", {x:{foo:true}}));
+console.log(3);
 assert.equal("<div>NO</div>",
   ht("<div ht-if='!x.bar'>NO</div>", {x:{foo:true}}));
 
 /*******************************************************
  * `ht-repeat` expression test
  *******************************************************/
+console.log(4);
 assert.equal("<li>1</li><li>2</li><li>3</li>",
   ht("<li ht-repeat='el in [1,2,3]'>{{el}}</li>", {}));
+console.log(5);
 assert.equal("<li>1</li><li>2</li><li>3</li>",
   ht("<li ht-repeat='el in list'>{{el}}</li>", {list:[1,2,3]}));
+console.log(6);
 assert.equal("<li>1</li><li>2</li><li>3</li>",
   ht(
     "<li ht-repeat='(k,v) in list'>{{v}}</li>",
     {list:{a:1, b:2, c:3}}
   ));
+console.log(7);
 assert.equal("<li>a1</li><li>b2</li><li>c3</li>",
   ht(
     "<li ht-repeat='(k,v) in list'>{{k}}{{v}}</li>", 
     {list:{a:1, b:2, c:3}}
   ));
 
-/*******************************************************
+/*******************************************************************
  * `ht-include` expression test
- *******************************************************/
-assert.equal("<div><%= include('file1.html', data) %></div>",
-  ht("<div ht-include=\"'file1.html'\"></div>", {}, true));
+ * file does not exist, so it will print out as html, the file name
+ *******************************************************************/
+console.log(8);
+assert.equal("<div>/Users/allen/github/angular-template/file1.html</div>",
+  ht("<div ht-include=\"file1.html\"></div>", {}, true));
